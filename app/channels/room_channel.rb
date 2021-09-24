6481f9460d45cb284f1da5940c98042f21dc5adc
kvo92 <88671104+kvo92@users.noreply.github.com>
2021-09-23 22:46:51 -0400

    finished first tutorial

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,10 @@
+# rails g channel room
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+     stream_from "room_channel_#{params[:room_id]}"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
-
+    has_many :messages
     validates_uniqueness_of :username
+
+    def self.generate
+        adjectives = ['Ancient', 'Broken', 'Creative', 'Dangerous', 'Effective', 'Flying', 'Gilded']
+        nouns  = ['Highway', 'Bird', 'Angel', 'Chopsticks', 'Master']
+        number = rand.to_s[2..6]
+        username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+        create(username: username)
+    end
 
 end
